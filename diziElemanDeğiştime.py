28fabb000388196d8cce1e663645a27d8f9515a7
Özgür BAYRAM <ozgurbayramdev@gmail.com>
2019-12-27 20:25:27 +0300

    second code added

--- diziElemanDeğiştime.py
+++ diziElemanDeğiştime.py
@@ -6,7 +6,9 @@ print(stirng.replace(stirng[0:5],"HELLO"))
 
 #Bir dizinin istenilen bir elemanını değiştirmek için ise aşağıdaki yöntem uygulanılabilir
 
+
+
 dizi1 = [0,1,2,3,4,5,6,7] 
 dizi2 = ["a","b","c","d","e","f","g"]
 
-print(list(map(lambda x: x if x !=  dizi1[0] else dizi2[0],dizi1 )))
+print(list(map(lambda x: x if x !=  dizi1[0] else dizi2[0],dizi1)))
