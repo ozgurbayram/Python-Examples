61357e4b32f2f03b00ef87b519e21332d837918d
ozgurbayram <ozgurbayram@protonmail.com>
2020-03-22 17:23:29 +0300

    src

--- Basics/MaxValueInList.py
+++
@@ -1,8 +0,0 @@
-dizi = [1,2,3,4,5]
-maxd = dizi[0]
-
-for i in dizi[0:4]:
-    if maxd<dizi[i]:
-        maxd = dizi[i]
-
-print(maxd)
--- Basics/passHashing.py
+++
@@ -1,6 +0,0 @@
-import bcrypt
-
-
-passwrd = 'hello3Bworld'.encode()
-
-print(bcrypt.hashpw(passwrd, bcrypt.gensalt(12)))
